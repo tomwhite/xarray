7331b5991ab2b6f040e2a3bb9b4fb3a91f23d006
Tom White <tom.e.white@gmail.com>
2024-08-07 14:35:26 +0100

    TestDaskOps

--- xarray/tests/test_duck_array_ops.py
+++ xarray/tests/test_duck_array_ops.py
@@ -261,10 +261,10 @@ class TestOps:
 class TestDaskOps(TestOps):
     @pytest.fixture(autouse=True)
     def setUp(self):
-        import dask.array
+        import cubed
 
-        self.x = dask.array.from_array(
-            [
+        self.x = cubed.from_array(
+            np.array([
                 [
                     [nan, nan, 2.0, nan],
                     [nan, 5.0, 6.0, nan],
@@ -275,7 +275,7 @@ class TestDaskOps(TestOps):
                     [nan, 17.0, 18.0, nan],
                     [nan, 21.0, nan, nan],
                 ],
-            ],
+            ]),
             chunks=(2, 1, 2),
         )
 
